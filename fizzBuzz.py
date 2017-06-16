aa66b6ee516f3f34f095fad95ae4e47c0bb0449c
Austin0077 <austinqweyu@gmail.com>
2017-06-16 22:33:37 +0300

    Add files via upload

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,16 +1,19 @@
 def fizzBuzz():
 	checkNum=0
 
-	while checkNum!=5 and checkNum!=3:
-		print('Input an Interger')
-		checkNum=input()
-		if int(checkNum)%5==0 and int(checkNum)%3==0:
-			print('Fizzbuzz')
-		elif int(checkNum)%5==0:
-			print('Buzz')
-		elif int(checkNum)%3==0:
-			print('Fizz')
-		else:
-			print(checkNum)
-
+while True:
+	print('Input an Interger')
+	checkNum=input()
+	rem=int(checkNum)%5
+	rem1=int(checkNum)%3
+	if int(checkNum)%5==0 and int(checkNum)%3==0:
+		print('Fizzbuzz')
+	elif int(checkNum)%5==0:
+		print('Buzz')
+	elif int(checkNum)%3==0:
+		print('Fizz')
+	else:
+		print(checkNum)
+	if rem==0 or rem1==0:
+		break
 fizzBuzz()
