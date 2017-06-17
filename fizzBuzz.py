913705547d7ed45e42b5aeb1715ece6d212bf222
James Lemayian <lemayiannakolah@gmail.com>
2017-06-17 22:56:18 +0300

    Just some minor corrections...
    Good that you used a break statement...although not very necessary.
    Your while loop was fine but did not run within the function because of the indentation. Remember that neatness is one of the python syntax rules? Otherwise, good work. See you on Monday for further discussion.

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,19 +1,19 @@
 def fizzBuzz():
-	checkNum=0
+    while True:
+        print('Input an Interger')
+        checkNum=input()
+        rem=int(checkNum)%5
+        rem1=int(checkNum)%3
+        if rem==0 and rem1==0:
+            print('Fizzbuzz')
+        elif rem==0:
+            print('Buzz')
+        elif rem1==0:
+            print('Fizz')
+        else:
+            print(checkNum)
+        if rem==0 or rem1==0:
+            break
 
-while True:
-	print('Input an Interger')
-	checkNum=input()
-	rem=int(checkNum)%5
-	rem1=int(checkNum)%3
-	if int(checkNum)%5==0 and int(checkNum)%3==0:
-		print('Fizzbuzz')
-	elif int(checkNum)%5==0:
-		print('Buzz')
-	elif int(checkNum)%3==0:
-		print('Fizz')
-	else:
-		print(checkNum)
-	if rem==0 or rem1==0:
-		break
+
 fizzBuzz()
